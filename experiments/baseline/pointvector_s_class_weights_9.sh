abc1391840ded8409c5efd8090ea26ec2fc1e750
Simon B. Jensen <simonbuusjensen@hotmail.com>
2025-01-28 10:28:54 +0100

    sync

--- experiments/baseline/pointvector_s_class_weights_9.sh
+++ experiments/baseline/pointvector_s_class_weights_9.sh
@@ -1,6 +1,6 @@
 
 
-python train_classifier.py --batch-size 8 --with_class_weights True --with_point_cloud_scaling True --with_point_cloud_rotations True --with_point_cloud_jitter True --num_points 8192 --project_name BioVista-Hyperparameter-Search
+python train_classifier.py --batch_size 8 --with_class_weights True --with_point_cloud_scaling True --with_point_cloud_rotations True --with_point_cloud_jitter True --num_points 8192 --project_name BioVista-Hyperparameter-Search
 python train_classifier.py --batch_size 8 --with_class_weights True --with_point_cloud_scaling True --with_point_cloud_rotations True --with_point_cloud_jitter True --num_points 8192 --project_name BioVista-Hyperparameter-Search
 
 
