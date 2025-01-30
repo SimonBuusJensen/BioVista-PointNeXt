a09702d97683891e59ffd150f9c01fa95382c82c
Simon B. Jensen <simonbuusjensen@hotmail.com>
2025-01-30 22:23:16 +0100

    Sync

--- experiments/BioVista-Hyperparameter-Search_v2/pointvector_s_class_weights_8.sh
+++ experiments/BioVista-Hyperparameter-Search_v2/pointvector_s_class_weights_8.sh
@@ -1,5 +1,5 @@
 
 
 
-python train_classifier.py --channels xyz  --num_points 24576 --qb_radius 0.7 --batch_size 16 --lr 0.0001 --project_name BioVista-Hyperparameter-Search-v2
-python train_classifier.py --channels xyz  --num_points 24576 --qb_radius 0.7 --batch_size 16 --lr 0.0001 --project_name BioVista-Hyperparameter-Search-v2
+python train_classifier.py --channels xyz  --num_points 24576 --qb_radius 0.7 --batch_size 12 --lr 0.0001 --project_name BioVista-Hyperparameter-Search-v2
+python train_classifier.py --channels xyz  --num_points 24576 --qb_radius 0.7 --batch_size 12 --lr 0.0001 --project_name BioVista-Hyperparameter-Search-v2
