c971faeaddb6f8d867986d2565836f77c87050c0
Simon B. Jensen <simonbuusjensen@hotmail.com>
2025-02-24 15:52:56 +0100

    Sync

--- examples/classification/train.py
+++ examples/classification/train.py
@@ -139,140 +139,140 @@ def main(gpu, cfg):
     
     cfg.classes = cfg.get('classes', None) or val_loader.dataset.classes if hasattr(val_loader.dataset, 'classes') else None or np.range(num_classes)
 
-    # """
-    # TRAINING
-    # """
-    # val_macc, val_oa, best_val_oa, best_epoch = 0., 0., 0., 0
-    # model.zero_grad()
+    """
+    TRAINING
+    """
+    val_macc, val_oa, best_val_oa, best_epoch = 0., 0., 0., 0
+    model.zero_grad()
 
-    # for epoch in range(cfg.start_epoch, cfg.epochs + 1):
+    for epoch in range(cfg.start_epoch, cfg.epochs + 1):
         
-    #     if hasattr(train_loader.dataset, 'epoch'):
-    #         train_loader.dataset.epoch = epoch - 1
+        if hasattr(train_loader.dataset, 'epoch'):
+            train_loader.dataset.epoch = epoch - 1
         
-    #     train_loss, train_macc, train_oa, _, train_cm = train_one_epoch(model, train_loader, optimizer, scheduler, epoch, cfg)
+        train_loss, train_macc, train_oa, _, train_cm = train_one_epoch(model, train_loader, optimizer, scheduler, epoch, cfg)
 
-    #     lr = optimizer.param_groups[0]['lr']
-    #     if cfg.wandb.use_wandb:
-    #         wandb.log({
-    #             "train_loss": train_loss,
-    #             "train_acc": train_macc,
-    #             "train_oa": train_oa,
-    #             "lr": lr,
-    #             "epoch": epoch
-    #         })
+        lr = optimizer.param_groups[0]['lr']
+        if cfg.wandb.use_wandb:
+            wandb.log({
+                "train_loss": train_loss,
+                "train_acc": train_macc,
+                "train_oa": train_oa,
+                "lr": lr,
+                "epoch": epoch
+            })
 
-    #     logging.info(f"Mean train acc (%): {train_macc:.1f}%, Train loss: {train_loss:.3f}, lr: {lr}")
-    #     for class_idx in range(train_cm.num_classes):
-    #         class_total_train = train_cm.actual[class_idx].item()
-    #         class_correct_train = train_cm.tp[class_idx].item()
-    #         class_acc_train = (class_correct_train / class_total_train) * 100 if class_total_train > 0 else 0
-    #         logging.info(f"Train: class {cfg.classes[class_idx]} (id: {class_idx}) correct: {class_correct_train}/{class_total_train} ({class_acc_train:.1f}%)")
+        logging.info(f"Mean train acc (%): {train_macc:.1f}%, Train loss: {train_loss:.3f}, lr: {lr}")
+        for class_idx in range(train_cm.num_classes):
+            class_total_train = train_cm.actual[class_idx].item()
+            class_correct_train = train_cm.tp[class_idx].item()
+            class_acc_train = (class_correct_train / class_total_train) * 100 if class_total_train > 0 else 0
+            logging.info(f"Train: class {cfg.classes[class_idx]} (id: {class_idx}) correct: {class_correct_train}/{class_total_train} ({class_acc_train:.1f}%)")
         
-    #     """
-    #     VALIDATION
-    #     """
-    #     is_best = False
-    #     val_loss_meter = AverageMeter()
-    #     if epoch % cfg.val_freq == 0:
+        """
+        VALIDATION
+        """
+        is_best = False
+        val_loss_meter = AverageMeter()
+        if epoch % cfg.val_freq == 0:
             
-    #         with torch.set_grad_enabled(False):
-    #             pred_list = []
-    #             conf_list = []
-    #             label_list = []
-    #             img_path_list = []
-    #             # Run validation
-    #             model.eval()  # set model to eval mode
+            with torch.set_grad_enabled(False):
+                pred_list = []
+                conf_list = []
+                label_list = []
+                img_path_list = []
+                # Run validation
+                model.eval()  # set model to eval mode
 
-    #             # Set no grad for validation
-    #             val_cm = ConfusionMatrix(num_classes=cfg.num_classes)
-    #             pbar = tqdm(enumerate(val_loader), total=val_loader.__len__())
-    #             for idx, (fn, data) in pbar:
-    #                 for key in data.keys():
-    #                     data[key] = data[key].cuda(non_blocking=True)
-    #                 target = data['y']
-    #                 points = data['x']
-    #                 points = points[:, :cfg.num_points]
-    #                 data['pos'] = points[:, :, :3].contiguous()
-    #                 data['x'] = points[:, :, :cfg.model.in_channels].transpose(1, 2).contiguous()
+                # Set no grad for validation
+                val_cm = ConfusionMatrix(num_classes=cfg.num_classes)
+                pbar = tqdm(enumerate(val_loader), total=val_loader.__len__())
+                for idx, (fn, data) in pbar:
+                    for key in data.keys():
+                        data[key] = data[key].cuda(non_blocking=True)
+                    target = data['y']
+                    points = data['x']
+                    points = points[:, :cfg.num_points]
+                    data['pos'] = points[:, :, :3].contiguous()
+                    data['x'] = points[:, :, :cfg.model.in_channels].transpose(1, 2).contiguous()
 
-    #                 # Forward pass
-    #                 logits, val_loss = model.get_logits_loss(data, target)
+                    # Forward pass
+                    logits, val_loss = model.get_logits_loss(data, target)
 
-    #                 val_cm.update(logits.argmax(dim=1), target)
-    #                 val_loss_meter.update(val_loss.item()) # Update validation loss meter
+                    val_cm.update(logits.argmax(dim=1), target)
+                    val_loss_meter.update(val_loss.item()) # Update validation loss meter
 
-    #                 # Save the predictions and labels
-    #                 pred_list.extend(logits.argmax(dim=1).cpu().numpy())
+                    # Save the predictions and labels
+                    pred_list.extend(logits.argmax(dim=1).cpu().numpy())
 
-    #                 confidences = torch.nn.functional.softmax(logits, dim=1)
-    #                 confidences = torch.max(confidences, 1)[0]
+                    confidences = torch.nn.functional.softmax(logits, dim=1)
+                    confidences = torch.max(confidences, 1)[0]
 
-    #                 conf_list.extend(confidences.cpu().numpy())
-    #                 label_list.extend(target.cpu().numpy())
-    #                 img_path_list.extend(fn)
+                    conf_list.extend(confidences.cpu().numpy())
+                    label_list.extend(target.cpu().numpy())
+                    img_path_list.extend(fn)
 
-    #             tp, count = val_cm.tp, val_cm.count
-    #             val_macc, val_oa, _ = val_cm.cal_acc(tp, count)
+                tp, count = val_cm.tp, val_cm.count
+                val_macc, val_oa, _ = val_cm.cal_acc(tp, count)
 
-    #             is_best = val_oa > best_val_oa
-    #             if is_best:
-    #                 best_val_oa = val_oa
-    #                 best_epoch = epoch
-    #                 logging.info(f'Found new best ckpt at epoch: @E{epoch}')
-    #                 save_checkpoint(cfg, model, epoch, optimizer, scheduler, additioanl_dict={'val_oacc': val_oa}, post_fix="ckpt_best")
+                is_best = val_oa > best_val_oa
+                if is_best:
+                    best_val_oa = val_oa
+                    best_epoch = epoch
+                    logging.info(f'Found new best ckpt at epoch: @E{epoch}')
+                    save_checkpoint(cfg, model, epoch, optimizer, scheduler, additioanl_dict={'val_oacc': val_oa}, post_fix="ckpt_best")
 
-    #                 # Write the results to a csv file
-    #                 # Write the image_paths, predictions and labels to a csv file
-    #                 pred_label_fp = os.path.join(cfg.run_dir, f"epoch_{epoch}_oacc_{round(best_val_oa, 2)}_val_pred_labels.csv")
-    #                 with open(pred_label_fp, "w") as f:
-    #                     f.write("image_path,prediction,label,correct,confidence\n")
-    #                     for img_path, pred, label, conf in zip(img_path_list, pred_list, label_list, conf_list):
-    #                         f.write(
-    #                             f"{os.path.basename(img_path)},{pred},{label},{int(pred == label)},{round(conf*100, 0)}\n")
-    #                     # Write overall high, low and total accuracy
-    #                     low_total = val_cm.actual[0].item()
-    #                     low_correct = val_cm.tp[0].item() 
-    #                     low_acc = (low_correct / low_total) * 100 if low_total > 0 else 0
-    #                     f.write(f"Low bio correct,{low_correct},{low_total},{low_acc}\n")
-    #                     high_total = val_cm.actual[1].item()
-    #                     high_correct = val_cm.tp[1].item()
-    #                     high_acc = (high_correct / high_total) * 100 if high_total > 0 else 0
-    #                     f.write(f"High bio correct,{high_correct},{high_total},{high_acc}\n")
-    #                     f.write(f"Overall validation accuracy,{val_cm.tp.sum().item()},{val_cm.actual.sum().item()},{val_oa}\n")
-    #                     f.write(f"Mean validation accuracy,,,{best_val_oa}")
-    #                 f.close()
+                    # Write the results to a csv file
+                    # Write the image_paths, predictions and labels to a csv file
+                    pred_label_fp = os.path.join(cfg.run_dir, f"epoch_{epoch}_oacc_{round(best_val_oa, 2)}_val_pred_labels.csv")
+                    with open(pred_label_fp, "w") as f:
+                        f.write("image_path,prediction,label,correct,confidence\n")
+                        for img_path, pred, label, conf in zip(img_path_list, pred_list, label_list, conf_list):
+                            f.write(
+                                f"{os.path.basename(img_path)},{pred},{label},{int(pred == label)},{round(conf*100, 0)}\n")
+                        # Write overall high, low and total accuracy
+                        low_total = val_cm.actual[0].item()
+                        low_correct = val_cm.tp[0].item() 
+                        low_acc = (low_correct / low_total) * 100 if low_total > 0 else 0
+                        f.write(f"Low bio correct,{low_correct},{low_total},{low_acc}\n")
+                        high_total = val_cm.actual[1].item()
+                        high_correct = val_cm.tp[1].item()
+                        high_acc = (high_correct / high_total) * 100 if high_total > 0 else 0
+                        f.write(f"High bio correct,{high_correct},{high_total},{high_acc}\n")
+                        f.write(f"Overall validation accuracy,{val_cm.tp.sum().item()},{val_cm.actual.sum().item()},{val_oa}\n")
+                        f.write(f"Mean validation accuracy,,,{best_val_oa}")
+                    f.close()
 
-    #             if cfg.wandb.use_wandb:
-    #                 wandb.log({
-    #                     "best_val_oacc": best_val_oa,
-    #                     "macc_when_best": val_macc,
-    #                     "epoch": epoch
-    #                 })
+                if cfg.wandb.use_wandb:
+                    wandb.log({
+                        "best_val_oacc": best_val_oa,
+                        "macc_when_best": val_macc,
+                        "epoch": epoch
+                    })
 
-    #             logging.info(f"Mean val acc (%): {val_macc:.1f}%, Val loss: {val_loss_meter.avg:.4f}, Val OA: {val_oa:.1f}%")
-    #             for class_idx in range(val_cm.num_classes):
-    #                 class_total_val = val_cm.actual[class_idx].item()
-    #                 class_correct_val = val_cm.tp[class_idx].item()
-    #                 class_acc_val = (class_correct_val / class_total_val) * 100 if class_total_val > 0 else 0
-    #                 logging.info(f"Val: class {cfg.classes[class_idx]} (id: {class_idx}) correct: {class_correct_val}/{class_total_val} ({class_acc_val:.1f}%)")
-    #                 if cfg.wandb.use_wandb:
-    #                     wandb.log({
-    #                         f"val_acc_{cfg.classes[class_idx]}": class_acc_val,
-    #                         "epoch": epoch
-    #                     })
+                logging.info(f"Mean val acc (%): {val_macc:.1f}%, Val loss: {val_loss_meter.avg:.4f}, Val OA: {val_oa:.1f}%")
+                for class_idx in range(val_cm.num_classes):
+                    class_total_val = val_cm.actual[class_idx].item()
+                    class_correct_val = val_cm.tp[class_idx].item()
+                    class_acc_val = (class_correct_val / class_total_val) * 100 if class_total_val > 0 else 0
+                    logging.info(f"Val: class {cfg.classes[class_idx]} (id: {class_idx}) correct: {class_correct_val}/{class_total_val} ({class_acc_val:.1f}%)")
+                    if cfg.wandb.use_wandb:
+                        wandb.log({
+                            f"val_acc_{cfg.classes[class_idx]}": class_acc_val,
+                            "epoch": epoch
+                        })
 
-    #     if cfg.wandb.use_wandb:
-    #         wandb.log({
-    #             "val_acc": val_macc,
-    #             "val_loss": val_loss_meter.avg,  # Log validation loss to wandb
-    #             "val_oa": val_oa,
-    #             "val_macc": val_macc,
-    #             "epoch": epoch
-    #         })
+        if cfg.wandb.use_wandb:
+            wandb.log({
+                "val_acc": val_macc,
+                "val_loss": val_loss_meter.avg,  # Log validation loss to wandb
+                "val_oa": val_oa,
+                "val_macc": val_macc,
+                "epoch": epoch
+            })
      
-    #     if cfg.sched_on_epoch:
-    #         scheduler.step(epoch)
+        if cfg.sched_on_epoch:
+            scheduler.step(epoch)
 
     """
     TESTING
