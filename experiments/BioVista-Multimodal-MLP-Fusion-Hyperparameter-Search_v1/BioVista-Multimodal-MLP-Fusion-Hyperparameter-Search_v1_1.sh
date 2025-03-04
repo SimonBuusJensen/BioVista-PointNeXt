d975313b34e1e4a6e80a403e45e176e48ed31cf7
SimonBuusJensen <simonbuusjensen@hotmail.com>
2025-03-04 15:27:32 +0100

    Init Multimodal MLP Fusion modal hyperparameter search

---
+++ experiments/BioVista-Multimodal-MLP-Fusion-Hyperparameter-Search_v1/BioVista-Multimodal-MLP-Fusion-Hyperparameter-Search_v1_1.sh
@@ -0,0 +1,2 @@
+
+python Train_MultiModalFusionModel.py --project_name BioVista-Multimodal-MLP-Fusion-Hyperparameter-Search_v1 --num_workers 8 --epochs 20 --batch_size 8 --lr 0.0001 --pointvector_weights --resnet_weights
