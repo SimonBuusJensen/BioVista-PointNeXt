cdd337df08f6a1212fa8a5b53dfd39cf91ee8575
Stefan Oehmcke <stefan.oehmcke@gmail.com>
2025-10-04 11:16:25 +0200

    fix val cm

--- examples/classification/train.py
+++ examples/classification/train.py
@@ -155,7 +155,7 @@ def main(gpu, cfg):
                 "train_loss": train_loss,
                 "train_acc": train_macc,
                 "train_oa": train_oa,
-                "train_cm": train_cm.get_wandb_table(train_loader.dataset.classes),
+                "train_cm": train_cm.get_wandb_table(cfg.num_classes),
                 "lr": lr,
                 "epoch": epoch
             })
@@ -267,7 +267,7 @@ def main(gpu, cfg):
                 "val_loss": val_loss_meter.avg,  # Log validation loss to wandb
                 "val_oa": val_oa,
                 "val_macc": val_macc,
-                "val_cm": val_cm.get_wandb_table(train_dataset.classes),
+                "val_cm": val_cm.get_wandb_table(cfg.num_classes),
                 "epoch": epoch
             })
      
